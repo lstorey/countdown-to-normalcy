b3bf82a1035bb61f9171783613a77b8a30aaeef7
Lisa Storey <lstorey@gmail.com>
2020-04-23 21:55:40 -0600

    Updates

---
+++ precache-manifest.b04417af7d4686f1304c729b2138f6a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2bf1d7848a68e47b90b2a49884a1fc",
+    "url": "/countdown-to-normalcy/index.html"
+  },
+  {
+    "revision": "b196b6df9ccbb56c330e",
+    "url": "/countdown-to-normalcy/static/css/main.4a1a3bfd.chunk.css"
+  },
+  {
+    "revision": "0aac31b1d2b42b136fff",
+    "url": "/countdown-to-normalcy/static/js/2.276a839c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countdown-to-normalcy/static/js/2.276a839c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b196b6df9ccbb56c330e",
+    "url": "/countdown-to-normalcy/static/js/main.25909b5b.chunk.js"
+  },
+  {
+    "revision": "89a0e4d4caf2f5c4d86c",
+    "url": "/countdown-to-normalcy/static/js/runtime-main.e8ec206d.js"
+  }
+]);
